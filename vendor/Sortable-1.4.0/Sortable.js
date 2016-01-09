61fe3edb18d243bd7d883075de090df343d91955
Mihkel Oviir <mihkel.oviir@gmail.com>
2016-01-09 17:15:17 +0200

    Improvement [#13] Update Sortable from 1.2.0 to 1.4.0

--- vendor/Sortable-1.4.0/Sortable.js
+++ vendor/Sortable-1.4.0/Sortable.js
@@ -1,21 +1,28 @@
-/**
- * jQuery plugin for Sortable
+/**!
+ * Sortable
  * @author	RubaXa   <trash@rubaxa.org>
  * @license MIT
  */
+
+
 (function (factory) {
 	"use strict";
 
 	if (typeof define === "function" && define.amd) {
-		define(["jquery"], factory);
+		define(factory);
+	}
+	else if (typeof module != "undefined" && typeof module.exports != "undefined") {
+		module.exports = factory();
+	}
+	else if (typeof Package !== "undefined") {
+		Sortable = factory();  // export for Meteor.js
 	}
 	else {
 		/* jshint sub:true */
-		factory(jQuery);
+		window["Sortable"] = factory();
 	}
-})(function ($) {
+})(function () {
 	"use strict";
-
 
 	var dragEl,
 		parentEl,
@@ -1236,45 +1243,7 @@
 	};
 
 
-	
-
-
-	/**
-	 * jQuery plugin for Sortable
-	 * @param   {Object|String} options
-	 * @param   {..*}           [args]
-	 * @returns {jQuery|*}
-	 */
-	$.fn.sortable = function (options) {
-		var retVal,
-			args = arguments;
-
-		this.each(function () {
-			var $el = $(this),
-				sortable = $el.data('sortable');
-
-			if (!sortable && (options instanceof Object || !options)) {
-				sortable = new Sortable(this, options);
-				$el.data('sortable', sortable);
-			}
-
-			if (sortable) {
-				if (options === 'widget') {
-					return sortable;
-				}
-				else if (options === 'destroy') {
-					sortable.destroy();
-					$el.removeData('sortable');
-				}
-				else if (typeof sortable[options] === 'function') {
-					retVal = sortable[options].apply(sortable, [].slice.call(args, 1));
-				}
-				else if (options in sortable.options) {
-					retVal = sortable.option.apply(sortable, args);
-				}
-			}
-		});
-
-		return (retVal === void 0) ? this : retVal;
-	};
+	// Export
+	Sortable.version = '1.3.0';
+	return Sortable;
 });
