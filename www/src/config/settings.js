394ea5584932987dcb71d48d1785c4d13c8d3dfa
Mihkel Oviir <mihkel.oviir@gmail.com>
2016-01-17 19:21:18 +0200

    Feature (#17): Discovered raster reprojection bug in ol3

--- www/src/config/settings.js
+++ www/src/config/settings.js
@@ -21,33 +21,57 @@ define(function () {
                     type: 'OSM',
                     title: 'OSM Eesti',
                     url: 'http://kaart.maakaart.ee/osm/tiles/1.0.0/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw',
-                    projection: 'EPSG:3857'
+                    projection: 'EPSG:3857',
+                    crossOrigin: null
                 },
                 osm : {
                     type: 'OSM',
                     title: 'OSM',
                     url: 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    projection: 'EPSG:3857'
+                    projection: 'EPSG:3857',
+                    crossOrigin: null
                 },
                 ma_kaart : {
                     type: 'Group',
                     title: 'MA Kaart',
-                    projection: 'EPSG:3301',
                     layers: [{
                         type: 'XYZ',
                         url: 'http://tiles.maaamet.ee/tm/s/1.0.0/kaart/{z}/{x}/{-y}.png',
-                        minResolution: 7.8125
+                        projection: 'EPSG:3301',
+                        minResolution: 7.8125,
+                        crossOrigin: null
                     }, {
                         type: 'XYZ',
                         url: 'http://tiles.maaamet.ee/tm/s/1.0.0/epk_vv/{z}/{x}/{-y}.png',
-                        maxResolution: 7.8125
+                        projection: 'EPSG:3301',
+                        minResolution: 0.8,
+                        maxResolution: 7.8125,
+                        crossOrigin: null
                     }]
                 },
                 ma_orto : {
-                    type: 'XYZ',
+                    type: 'Group',
                     title: 'MA Foto',
-                    url: 'http://tiles.maaamet.ee/tm/s/1.0.0/foto/{z}/{x}/{-y}.jpg',
-                    projection: 'EPSG:3301'
+                    layers: [{
+                        type: 'XYZ',
+                        url: 'http://tiles.maaamet.ee/tm/s/1.0.0/foto/{z}/{x}/{-y}.jpg',
+                        projection: 'EPSG:3301',
+                        minResolution: 0.8,
+                        crossOrigin: null
+                    }, {
+                        type: 'TileWMS',
+                        url: 'http://kaart.maaamet.ee/wms/fotokaart',
+                        params: {
+                            LAYERS: 'EESTIFOTO',
+                            TILED: true,
+                            FORMAT: 'image/png',
+                            VERSION: '1.1.1'
+                        },
+                        projection: 'EPSG:3301',
+                        maxResolution: 0.8,
+                        gutter: 20,
+                        crossOrigin: null
+                    }]
                 }
             },
             'activeBaseLayer' : 'osm_et',
