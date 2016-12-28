fa8961c249b57bc9de4cba29add1af740b2827da
Mihkel Oviir <mihkel.oviir@gmail.com>
2016-12-28 23:33:14 +0200

    [#20] gh-pages deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -70,7 +70,7 @@ parse_args() {
 	# vars should be declared here, with sane defaults if applicable.
 
 	# Source directory & target branch.
-	deploy_directory=${"www/dist"}
+	deploy_directory=${"www/dist":-dist}
 	deploy_branch=${GIT_DEPLOY_BRANCH:-gh-pages}
 
 	#if no user identity is already set in the current git environment, use this:
