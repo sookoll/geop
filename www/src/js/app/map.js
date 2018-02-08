2776d14fa69262cb16058f27bad315f081759d6d
Mihkel Oviir <mihkel.oviir@gmail.com>
2018-02-08 23:14:31 +0200

    [#25] measurements improvements

--- www/src/js/app/map.js
+++ www/src/js/app/map.js
@@ -268,7 +268,7 @@ define([
                     zoom: false
                 }),
                 target: document.getElementById(_this._config.el),
-                moveTolerance: 3,
+                moveTolerance: 10,
                 view: new ol.View({
                     projection: 'EPSG:3857',
                     center: _this.transform('point', permalink.center, 'EPSG:4326', 'EPSG:3857'),
