601eab524372ad7a09c3d2ce1e609bc6b1adcde7
Mihkel Oviir <mihkel.oviir@gmail.com>
2019-12-06 23:43:42 +0200

    publish: npm update
    generated from commit 4d20d0b514a63b330a55fa3a33da5e75b518bffd

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.73f5524399ae3eb8e6c4f0c4b6fd1d18.js"
+  "precache-manifest.28518b8a04e056ec970b2bcf258d5af9.js"
 );
 
 workbox.skipWaiting();
