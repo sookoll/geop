5f6d08c7cb2cfee2afca99465107349172e23865
Mihkel Oviir <mihkel.oviir@gmail.com>
2019-12-08 23:50:08 +0200

    WMTS support

--- src/config/layers.js
+++ src/config/layers.js
@@ -1,6 +1,20 @@
 export const layers = {
   base: [
     {
+      id: 'soome',
+      type: 'WMTS',
+      title: 'Soome',
+      url: 'https://mapservices-a.navici.com/basemaps/gwc/service/wmts?apikey=gdqWSTM10Le9XVAX8B6vFKxnpZAmTx5x',
+      projection: 'EPSG:3067',
+      layer: 'basemaps:rk',
+      matrixSet: 'ETRS-TM35FIN',
+      scaleDenominator: 2.925714285714286E7,
+      topLeftCorner: [-2097152.0, 9437184.0],
+      matrixWidth: 3,
+      matrixHeight: 2,
+      format: 'image/png8',
+      crossOrigin: null
+    }, {
       id: 'osm',
       type: 'XYZ',
       title: 'OSM',
