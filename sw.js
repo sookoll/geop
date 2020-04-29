cf4dc2d928e7d0793a8d9d4b7ec75643ab665284
Mihkel Oviir <mihkel.oviir@gmail.com>
2020-04-30 00:52:57 +0300

    publish: #99 fix search by moving cache layer back to layers. Fix ma pohikaart not showing up
    generated from commit 6670a6eeacc41d16a2e87c4df3a7cfa8cdeeb3be

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f43244a18694fede4285f11de9640eba.js"
+  "precache-manifest.a023a727553a061a0e91d893abfff814.js"
 );
 
 workbox.skipWaiting();
