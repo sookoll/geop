64701fe4aa6415a491ab3b9147286ce8ada915ea
Mihkel Oviir <mihkel.oviir@gmail.com>
2021-02-01 00:43:06 +0200

    publish: Version up
    generated from commit 028b82997a83a2269f098dd57a0240b48cef67ba

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.bd0737ea09f367105f3ee563edfcacab.js"
+  "precache-manifest.c45478eb9a204586bf2d91d8e90f2f1c.js"
 );
 
 workbox.skipWaiting();
