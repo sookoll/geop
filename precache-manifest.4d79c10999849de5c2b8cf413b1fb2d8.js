f3a62af168b6817b4fd755e693d95c2320b3f7ee
Mihkel Oviir <mihkel.oviir@gmail.com>
2022-08-03 16:28:22 +0300

    publish: #135 Layer labels
    generated from commit 41d50ab57e4a4e0b74d74c010e9cae524835cf2b

---
+++ precache-manifest.4d79c10999849de5c2b8cf413b1fb2d8.js
@@ -0,0 +1,93 @@
+self.__precacheManifest = [
+  {
+    "revision": "c05c347dac9492ec0c74",
+    "url": "main.css"
+  },
+  {
+    "revision": "c05c347dac9492ec0c74",
+    "url": "main.js"
+  },
+  {
+    "revision": "f0f424b12efde2ac99fc",
+    "url": "vendors~main.css"
+  },
+  {
+    "revision": "f0f424b12efde2ac99fc",
+    "url": "vendors~main.js"
+  },
+  {
+    "url": "assets/d28c96cabe5302f1b2c90275cd3f4e69.eot"
+  },
+  {
+    "url": "assets/dfe5aa4344a2d8a29aec8d83fb3fb14e.ttf"
+  },
+  {
+    "url": "assets/0232fe8b09de812c88e6cc27f57a3a88.eot"
+  },
+  {
+    "url": "assets/0ce1e868452204695c8ac1c70f693c2d.woff"
+  },
+  {
+    "url": "assets/0c9f225e8f69c622f681cf1ed973cc3d.woff2"
+  },
+  {
+    "url": "assets/3cace4a04d941b5981ba32c6ce9afae1.woff"
+  },
+  {
+    "url": "assets/847712aaabbeba674afdda86d31cab17.woff2"
+  },
+  {
+    "url": "assets/398be22b2308585b84da8e858e2d7b60.ttf"
+  },
+  {
+    "url": "assets/d0fdc7c20b0b0b1aaf27327547735fa3.svg"
+  },
+  {
+    "url": "assets/9ae050d1876ac1763eb6afe4264e6d5a.woff2"
+  },
+  {
+    "url": "assets/00bb62bc33034479d5e82a0fd1c5efe3.eot"
+  },
+  {
+    "url": "assets/4bfbf7eb4b19d9ff9293eb177b6d0070.woff"
+  },
+  {
+    "url": "assets/260d033f79f75d670fd09d8cf7c8bf8d.ttf"
+  },
+  {
+    "url": "assets/5d10d00980077f67990f73b1961bc46e.svg"
+  },
+  {
+    "url": "assets/2306f642b31b774ba1d8b6b784539e19.svg"
+  },
+  {
+    "revision": "ecb344a7b9fe5ecbc3c719fa479f3d6b",
+    "url": "CNAME"
+  },
+  {
+    "revision": "240a5f92ae0ac23e4c15e5e940d09ecc",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "1ed59a26c1045406e7d607d94656d42a",
+    "url": "index.html"
+  },
+  {
+    "url": "assets/icon_512x512.6e3f8c34c1e9a12240e4d56bc6935b78.png"
+  },
+  {
+    "url": "assets/icon_384x384.3549f042016f434c5642146c062a3a46.png"
+  },
+  {
+    "url": "assets/icon_256x256.22a34a38e39511889b2c37fa2961e3aa.png"
+  },
+  {
+    "url": "assets/icon_192x192.febdc38c3cb29a16f48aaef1e57da1c6.png"
+  },
+  {
+    "url": "assets/icon_128x128.a9c33e48b431cd6844290f814776053a.png"
+  },
+  {
+    "url": "assets/icon_96x96.64a8394c3dbf65535f09daf414d151bc.png"
+  }
+];
