daac55a143d2e359d9e182b0ecb961b34473b654
Mihkel Oviir <mihkel.oviir@gmail.com>
2024-03-26 11:32:58 +0200

    Update cadaster service

--- src/config/settings.js
+++ src/config/settings.js
@@ -55,13 +55,9 @@ export const apiUrls = {
     directions: 'https://www.google.com/maps/dir/?api=1&destination='
   },
   wmsexamples: {
-    'Kataster (WMS)': 'http://kaart.maaamet.ee/wms/alus?layers=TOPOYKSUS_6569,TOPOYKSUS_7793&SRS=EPSG:3301&title=Kataster',
+    'Kataster (WMS)': 'https://gsavalik.envir.ee/geoserver/kataster/wms?layers=ky_kehtiv,ky_aadress&srs=EPSG:3301&title=Kataster',
     'Looduskaitse (WMS)': 'https://gsavalik.envir.ee/geoserver/eelis/ows?layers=kr_kaitseala,kr_hoiuala,kr_hooldatav_skv,kr_looduslik_skv,kr_reservaat,kr_vep,plk,kr_kohalik_objekt_pv,kr_yksikobjekti_kaitsetsoon,kr_kohalik_objekt,kr_yksikobjektid&SRS=EPSG:3301&opacity=0.9&title=Looduskaitse',
-    'Pärandkultuur (WMS)': 'https://gsavalik.envir.ee/geoserver/eelis/ows?layers=pk_objekt_metsas&SRS=EPSG:3301&title=Pärandkultuur',
-    'LV foto (WMS)': 'https://wmsbm3.kartes.lv/BMdMiAsVp/wgs/orto/?title=LV orto&layers=0&srs=EPSG:3857',
-    'LV kaart (WMS)': 'https://wmsbm3.kartes.lv/BMdMiAsVp/wgs/15/?title=LV&layers=0&srs=EPSG:3857',
-    'Soome kaart (WMTS)': 'https://mapservices-a.navici.com/basemaps/gwc/service/wmts?apikey=gdqWSTM10Le9XVAX8B6vFKxnpZAmTx5x&title=Soome kaart&layer=basemaps:rk&srs=EPSG:3067&matrixSet=ETRS-TM35FIN&matrixTemplate=ETRS-TM35FIN:{z}&scaleDenominator=2.925714285714286E7&topLeftCorner=-2097152,9437184&matrixWidth=3&matrixHeight=2&format=image/png8',
-    'Soome foto (WMTS)': 'https://www.retkikartta.fi/wmts/30c616a00f157e7357721900e8b0415c/?srs=EPSG:3067&title=Soome foto&format=image/jpeg&layer=ortokuva&matrixSet=ETRS-TM35FIN&scaleDenominator=29257142.85714286&matrixWidth=1&matrixHeight=1&topLeftCorner=-548576,8388608'
+    'Pärandkultuur (WMS)': 'https://gsavalik.envir.ee/geoserver/eelis/ows?layers=pk_objekt_metsas&SRS=EPSG:3301&title=Pärandkultuur'
   }
 }
 export const geocache = {
